9fc63375561007ab93b3c2db7eeddc07ed2ea71a
Andrei Mihailescu <andrei.mihailescu994@gmail.com>
2018-12-30 14:08:17 +0200

    After Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,7 +9,8 @@ import {HeroService} from "../hero.service";
 })
 export class HeroesComponent implements OnInit {
     heroes: Hero[];
-    selectedHero: Hero;
+
+    // selectedHero: Hero;
 
     constructor(private heroService: HeroService) {
     }
@@ -22,8 +23,8 @@ export class HeroesComponent implements OnInit {
         this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
     }
 
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
+    // onSelect(hero: Hero): void {
+    //     this.selectedHero = hero;
+    // }
 
 }
